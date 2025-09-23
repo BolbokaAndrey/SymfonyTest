79c197a5d9f7ff05c618fb3e01bc6d6ec1f80ab0
Andrey Bolboka <bolboka.a@yandex.ru>
2025-09-24 02:34:53 +0300

    fix(properties): add news edit and new and fix templates and styles

--- src/Controller/AdminContentController.php
+++ src/Controller/AdminContentController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use App\Entity\PropertyDefinition;
+use App\Entity\PropertyValue;
 use App\Repository\NewsRepository;
 use App\Repository\PropertyDefinitionRepository;
+use App\Repository\PropertyValueRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +20,8 @@ final class AdminContentController extends AbstractController
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly NewsRepository $newsRepository,
+        private readonly PropertyValueRepository $propertyValueRepository,
+        private readonly PropertyDefinitionRepository $propertyDefinitionRepository,
     )
     {
     }
@@ -60,6 +67,66 @@ final class AdminContentController extends AbstractController
         return $this->render('admin_content/news.html.twig', [
             'title' => 'Новости',
             'news' => $formattedNews
+        ]);
+    }
+
+    #[Route('/news/new', name: '_news_new')]
+    public function new(Request $request): Response
+    {
+        $news = new News();
+        $property = new PropertyValue();
+        if ($request->isMethod('POST')) {
+
+            $news->setTitle($request->request->get('title'));
+            $news->setText($request->request->get('text'));
+            $news->setCreatedAt(new \DateTimeImmutable($request->request->get('createdAt')));
+            $property->setValue($request->request->get('source'));
+            $property->setPropertyDefinition($this->propertyDefinitionRepository->findOneBy(['code' => 'source']));
+            $news->addProperty($property);
+
+            $this->entityManager->persist($property);
+            $this->entityManager->persist($news);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Новость успешно создана');
+            return $this->redirectToRoute('admin_content_news');
+        }
+
+        return $this->render('admin_content/news_form.html.twig', [
+            'title' => 'Создание новости',
+            'propertyDefinition' => $news
+        ]);
+    }
+
+    #[Route('/news/{id}/edit', name: '_news_edit')]
+    public function edit(int $id, Request $request): Response
+    {
+        $news = $this->newsRepository->find($id);
+        $properties = $news->getProperties();
+        if (!$news) {
+            throw $this->createNotFoundException('Новость не найдена');
+        }
+
+        if ($request->isMethod('POST')) {
+            $news->setTitle($request->request->get('title'));
+            $news->setText($request->request->get('text'));
+            $news->setCreatedAt(new \DateTimeImmutable($request->request->get('createdAt')));
+            foreach ($properties as $property) {
+                $propertyCode = $property->getPropertyDefinition()->getCode();
+                match ($propertyCode) {
+                    'source' => $property->setValue($request->request->get('source')),
+                };
+            }
+
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Новость успешно обновлена');
+            return $this->redirectToRoute('admin_content_news');
+        }
+
+        return $this->render('admin_content/news_form.html.twig', [
+            'title' => 'Редактирование новости',
+            'news' => $news,
         ]);
     }
 }
